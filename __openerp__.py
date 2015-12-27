407219e48c27329b8eeb63ae573840046aed00a9
adrian.paesani@gmail.com <adrian.paesani@gmail.com>
2015-12-26 22:41:04 -0300

    Advanced Views

--- __openerp__.py
+++ __openerp__.py
@@ -29,6 +29,7 @@
         # 'security/ir.model.access.csv',
         'templates.xml',
         'views/openacademy.xml',
+        'views/partner.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
